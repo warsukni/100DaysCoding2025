08c5e2b8b8268582f5e0c80e1000cff04c47ae67
Warsukni <warsukniuni62@gmail.com>
2025-09-09 14:19:52 +0800

    Create Day2.java

---
+++ Day2.java
@@ -0,0 +1,12 @@
+public class warsukni_D0225002 {
+  public static void main(string[] args) {
+  //penggunaan println
+  System.out.println("nama: warsukni");
+  System.out.println("umur: 18 tahun");
+  //penggunaan pmrint  
+  System.out.print("hobi saya adalah menyanyi");
+  //penggunaan printf
+  System.out.printf("jumlah SKS :%d %n", 20);
+  System.out.printf("status :%s", "aktif");
+  }
+}
