18d89e5cde525f279101484c727e9cf9bd69f6bd
Warsukni <warsukniuni62@gmail.com>
2025-09-11 13:17:14 +0800

    Create Day4. escape-sequence.java

---
+++ Day4. escape-sequence.java
@@ -0,0 +1,16 @@
+public class WarsuknD0225002 {
+
+    public static void main(String[] args) {
+        // \n = newline 
+        System.out.println("haii, teman-teman. \nsaya warsukni");
+        
+        // \t = tab 
+        System.out.println("nama : \twarsukni");
+        System.out.println("kelas : \tA informatika");
+        
+        // \n = newline (digunakan untuk pindah baris baru)
+        // \t = tab (menambah spasi lebar seperti membuat tabel)
+
+        
+    }
+}
