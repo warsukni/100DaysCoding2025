ebe7d3f8f53e046f55fc50854285f5a6a8315748
Warsukni <warsukniuni62@gmail.com>
2025-09-13 20:21:22 +0800

    Create day6.java

---
+++ day6.java
@@ -0,0 +1,15 @@
+public class WARSUKN {
+    public static void main(String[] args) {
+        float hargabuku = 30.000f;
+        double hargabuku1 =  30.000;
+        int  jumlahbuku = 2;
+        //hitungan total harga
+        float total = hargabuku*jumlahbuku;
+        double total1 =hargabuku1*jumlahbuku;
+        
+        System.out.println("float,total harga : Rp" + total);
+        System.out.println("double, total harga : Rp" + total1);
+        
+        
+    }
+}
