8695bf02c5d873e74345792b1fc1edeee7d73a74
Warsukni <warsukniuni62@gmail.com>
2025-09-14 21:07:05 +0800

    Create day7,char,bolean.java

---
+++ day7,char,bolean.java
@@ -0,0 +1,14 @@
+public class UNI {
+    public static void main(String[] args) {
+        String nama = "warsukni";
+        int umur = 18;
+        char jeniskelamin = 'p';
+        boolean Mahasiswa = true;
+        
+        System.out.println("nama : "+ nama);
+        System.out.println("umur : "+ umur + "tahun");
+        System.out.println("jeniskelamin : " + jeniskelamin);
+        System.out.println("mahasiswa : statusMahasiwa");
+    }
+    
+}
