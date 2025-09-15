083a89b42c63313adf58cb09cadaf477ccac45cd
Warsukni <warsukniuni62@gmail.com>
2025-09-15 23:21:18 +0800

    Create day8penggunaanString.java

---
+++ day8penggunaanString.java
@@ -0,0 +1,17 @@
+public class warsukni {
+    public static void main(String[] args) {
+        String nama = "warsukni";
+        String hobi = "menyanyi";
+        String kampus = "universitas silawesi barat";
+        
+        //string dalam teks
+        System.out.println("halo, nama saya " + nama );
+        System.out.println("hobi saya " + hobi);
+        System.out.println("saya kuliah di " + kampus);
+        
+        //menggabungkan String menjadi kalimat
+        String kalimat = nama + " suka " + hobi + " di " + kampus;
+        System.out.println("kalimat gabungan: " + kalimat);
+    }
+    
+}
