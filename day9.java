162ec5518a8c0e889873069ff96d77023eaab592
Warsukni <warsukniuni62@gmail.com>
2025-09-16 21:18:12 +0800

    Create day9.java

---
+++ day9.java
@@ -0,0 +1,18 @@
+public class warsukni {
+    public static void main(String[] args) {
+    // Deklarasi variabel tanpa nilai
+    int umur;
+    String nama;
+    double tinggibadan;
+    //memberikan nilai ke variabel (inisialisasi)
+    umur = 18;
+    nama = "warsukni";
+    tinggibadan = 149.5;
+    // menampilkan nilai variabel
+        System.out.println("nama: " + nama);
+        System.out.println("umur: " + umur);
+        System.out.println("tinggi: " + tinggibadan + "cm");
+    
+    }
+    
+}
