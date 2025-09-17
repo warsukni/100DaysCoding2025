90e90aab7954f7e4f798f9c70b346dd8d1cf9da7
Warsukni <warsukniuni62@gmail.com>
2025-09-17 19:32:00 +0800

    Create Day10.java

---
+++ Day10.java
@@ -0,0 +1,9 @@
+package unii;
+public class java {
+    public static void main(String[] args) {
+        final String nama = "unii";
+        final int umur = 18;
+        System.out.println("nama panggilan saya " + nama);
+        System.out.println("umur sya saat ini " + umur);
+    }
+}
