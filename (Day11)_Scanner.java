3c78218b81fd095a401109787067ceed65da83b9
Warsukni <warsukniuni62@gmail.com>
2025-09-18 21:21:53 +0800

    Create (Day11)_Scanner.java

---
+++ (Day11)_Scanner.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class warsukni {
+    public static void main(String[] args) {
+        Scanner ah = new Scanner(System.in);
+        System.out.println("masukkan kredit");
+        String h = ah .nextLine();
+    }
+}
