d5f13d412b4e34d621fb49868eda708c2e5e8d14
Warsukni <warsukniuni62@gmail.com>
2025-09-21 19:40:47 +0800

    Create Day13_evaluasiMentor1.java

---
+++ Day13_evaluasiMentor1.java
@@ -0,0 +1,76 @@
+public class Warsukn {
+     public static void main(String[] args) {
+        //9 tipe data dengan masing masing memiliki 2 variabel
+        int umur = 2;
+        int saudara = 10;
+        byte a = 19;
+        byte b = 20;
+        long angka1 = 1234;
+        long angka2 = 5678;
+        double bb = 45.0;
+        double tb = 149;
+        char un = 'a';
+        char ni = 'b';
+        String nama = "warsukni";
+        String alamat = "rusung - rusung";
+        float na = 12;
+        float be = 15;
+        short luas = 255;
+        short tinggi = 266;
+        boolean benar = true;
+        boolean salah = false;
+        
+        System.out.println(umur);
+        System.out.println(saudara);
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(angka1);
+        System.out.println(angka2);
+        System.out.println(bb);
+        System.out.println(tb);
+        System.out.println(un);
+        System.out.println(ni);
+        System.out.println(nama);
+        System.out.println(alamat);
+        System.out.println(na);
+        System.out.println(be);
+        System.out.println(luas);
+        System.out.println(tinggi);
+        System.out.println(benar);
+        System.out.println(salah);
+        
+        umur = 18;
+        saudara = 1;
+        a = 15;
+        b = 16;
+        angka1 = 01234;
+        angka2 = 56789;
+        nama = "warsukni";
+        alamat = "rusung-rusung";
+        bb = 44.0;
+        tb = 150.0;
+        un = 'b';
+        ni = 'a';
+        benar = true;
+        salah = false;
+        
+        // cetak dengan println/print/printf
+        System.out.println(umur);
+        System.out.println(saudara);
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(angka1);
+        System.out.println(angka2);
+        System.out.printf(("%s"),nama);
+        System.out.printf(("\n%s"),alamat);
+        System.out.println(bb);
+        System.out.println(tb);
+        System.out.println(un);
+        System.out.println(ni);
+        System.out.print("\nbenar");
+        System.out.print("\nsalah");
+        
+        
+        
+    }
+}
