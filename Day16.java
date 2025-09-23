bf9190e75bd5db3f7e56b869904c6564f34e8ce7
Warsukni <warsukniuni62@gmail.com>
2025-09-23 20:29:49 +0800

    Create Day16.java

---
+++ Day16.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class day16 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Masukkan angka: ");
+        byte no1 = in.nextByte();
+        System.out.print("Masukkan angka: ");
+        byte no2 = in.nextByte();
+        System.out.println();
+        System.out.println("Nilai perkalian = "+(no1+no2));
+        System.out.println("Nilai pembagian = "+(no1-no2));
+    }
+    
+}
