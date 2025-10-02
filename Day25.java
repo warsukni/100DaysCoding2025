57e39e3c6a6b60e3d01b9afc5b694a278e18b8e3
Warsukni <warsukniuni62@gmail.com>
2025-10-02 21:41:14 +0800

    Create Day25.java

---
+++ Day25.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Day25 {
+    public static void main(String[] args) {
+        Scanner sc =  new Scanner(System.in);
+        int x = sc.nextInt();
+        System.out.println("x++");
+        System.out.println("++x");
+        System.out.println("x-");
+        System.out.println("-x");
+    }
+}
