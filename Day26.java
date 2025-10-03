26e6ed779796716db0de307484e0c416b2f62088
Warsukni <warsukniuni62@gmail.com>
2025-10-03 23:10:28 +0800

    Create Day26.java

---
+++ Day26.java
@@ -0,0 +1,17 @@
+public class Day25 {
+    public static void main(String[] args) {
+        int a = 10;
+        System.out.println("nilai awal a:" +a);
+        a+= 5;
+        System.out.println("setelah a+=5:"+a);
+        a-= 3;
+        System.out.println("setelah a-=3:+a");
+        a*=2;
+        System.out.println("setelah a*=2:+a");
+        a/=4;
+        System.out.println("setelah a/=4:+a");
+        a%=3;
+        System.out.println("setelah a%=3:+a");
+        
+    }
+}
