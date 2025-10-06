e9a40a12881a2cd6ffc3cb7b4f8308b03432f877
Warsukni <warsukniuni62@gmail.com>
2025-10-06 23:18:41 +0800

    Create Day29.java

---
+++ Day29.java
@@ -0,0 +1,9 @@
+public class Day29 {
+    public static void main(String[] args) {
+        int x = 10 ,y = 5;
+        System.out.println("x == y :" + (x == y));
+        System.out.println("x != y : " + (x !=y));
+        System.out.println("x>y :"+(x>y));
+        System.out.println("x<y :"+ (x<y));
+    }
+}
