616fb0ce6988a9d8f44d8083f84aad6c15eec190
Warsukni <warsukniuni62@gmail.com>
2025-10-09 21:53:24 +0800

    Create Day32.java

---
+++ Day32.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Day32 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner (System.in);
+        System.out.print("a:");
+        int a = in.nextInt();
+        System.out.print("b:");
+        int b = in.nextInt();
+        
+        System.out.println("a > 5 || b > 5");
+    }
+}
