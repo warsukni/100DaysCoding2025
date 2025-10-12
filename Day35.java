ad4891f80179df3eadc9437420287ec5a1e701d7
Warsukni <warsukniuni62@gmail.com>
2025-10-12 23:23:50 +0800

    Create Day35.java

---
+++ Day35.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Day35 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("masukkan angka");
+        int angka = input.nextInt();
+        if (angka > 0) {
+            System.out.println("angka positif");
+        }
+        System.out.println("program selesai");
+    }
+}
